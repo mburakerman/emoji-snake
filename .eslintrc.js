aaf9340cb78f920fa6da14486a59d95fe2120419
Mehmet Burak Erman <mburakerman@gmail.com>
2023-07-23 18:32:34 +0300

    eslint setup

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: [
+    "react",
+    "@typescript-eslint",
+    "prettier",
+    "react-hooks",
+    "functional",
+  ],
+  rules: {},
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
