273cb6ac8252c07e37fcf5f74e2113442c0ef961
Mehmet Burak Erman <mburakerman@gmail.com>
2023-08-04 16:54:53 +0300

    *

--- src/hooks/useBestScores.ts
+++ src/hooks/useBestScores.ts
@@ -1,5 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 
 import db from "../firebase";
 import { GameDifficulty } from "../Snake";
