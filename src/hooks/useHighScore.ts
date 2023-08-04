5161558ca2efb09309d48105f232339c6d09355f
Mehmet Burak Erman <mburakerman@gmail.com>
2023-08-04 16:23:10 +0300

    *

--- src/hooks/useHighScore.ts
+++ src/hooks/useHighScore.ts
@@ -1,24 +1,20 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from "react";
 import db from "../firebase";
 
 export const useHighScore = () => {
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<any>(null);
 
   const addNewHighScore = async (scoreData: any) => {
     try {
       setLoading(true);
-      setError(null);
 
       await db.collection("scores").add(scoreData);
     } catch (err) {
-      setError(err);
+      console.error(err);
     } finally {
       setLoading(false);
     }
   };
 
-  return { addNewHighScore, loading, error };
+  return { addNewHighScore, loading };
 };
